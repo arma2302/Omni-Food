70d330935ef197ebfaa702d2f0e50ca76bb0d7ef
arma2302 <126476531+arma2302@users.noreply.github.com>
2023-02-27 19:25:18 +0530

    scroll behaviour

--- js/script.js
+++ js/script.js
@@ -7,3 +7,48 @@ const headerEl = document.querySelector("header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// smooth scrolling
+// let navlinks = document.querySelectorAll("header nav a");
+
+// for (const link of navlinks) {
+//   link.addEventListener("click", smoothScroll);
+// }
+
+// function smoothScroll(event) {
+//   event.preventDefault();
+
+//   const href = this.getAttribute("href");
+
+//   document.querySelector(href).scrollIntoView({
+//     behavior: "smooth",
+//   });
+// }
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
